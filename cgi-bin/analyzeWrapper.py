707c3ed0aa1bb0a33cdcf866e0b24ce8403e65b5
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2014-04-20 01:04:36 +0200

    Prepped the wrapper for on server calling

--- cgi-bin/analyzeWrapper.py
+++ cgi-bin/analyzeWrapper.py
@@ -1,14 +1,19 @@
+#!/Python27/python
+
+import cgi, cgitb 
+
 import crawler
 import preprocess
 #import create_model
+
+cgitb.enable()  # for troubleshooting
+#the cgi library gets vars from html
+params = cgi.FieldStorage()
+
+analyze(params["username"], params["token"])
 
 def analyze(username, token):
 	limit = 1000
 	data = crawler.crawl(username, token, limit)
     processedData = preprocess.preprocess(data)
     #(header, clf) = create_model.build_model('processed-data.csv')
-
-if __name__ == '__main__':
-    token = ''
-    username = 'philip.anderson1'
-    analyze(username, token)
