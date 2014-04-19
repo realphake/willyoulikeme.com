09172d55ef5e6cca8afabada90470694ac32d22f
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2014-04-19 20:02:54 +0200

    Current time is taken into account!

--- js/input-field.js
+++ js/input-field.js
@@ -2,6 +2,7 @@ var time = 0;
 var prevTerms = [];
 var prevMetadata = {};
 var prevLikes = 0;
+var prevHours = 0;
 
 // update function
 function updateVisuals() {
@@ -14,16 +15,19 @@ function updateVisuals() {
 	
 		var photoSelected = document.getElementById('photo').checked * 1;
 		var linkSelected = document.getElementById('link').checked * 1;
+		var hours = new Date().getUTCHours();
 
 		// match message to data
-		metadata = {night:1,morning:0,afternoon:0,evening:0,link:linkSelected,photo:photoSelected};
+		metadata = {night:between(0,hours,6)*1,morning:between(6,hours,12)*1,
+				afternoon:between(12,hours,18)*1,evening:between(18,hours,24)*1,
+				link:linkSelected,photo:photoSelected};
 		dataset = match(message, metadata, data);
 
 		$('.highlighter').html(dataset.terms.join(""));
 		
 		// act if terms changed
 		if(dataset.matchingTerms.toString() != prevTerms.toString() || 
-				!objectEquals(metadata, prevMetadata) ) {
+				!objectEquals(metadata, prevMetadata) || hours != prevHours ) {
 			jQuery({animlikes: prevLikes}).animate({animlikes: dataset.likes}, {
 				duration: 1000,
 				easing: 'swing', // can be anything
@@ -36,6 +40,7 @@ function updateVisuals() {
 			prevTerms = dataset.matchingTerms.slice(0);
 			prevMetadata = metadata;
 			prevLikes = dataset.likes;
+			prevHours = hours;
 
 			// reset filtered friends if message is empty
 			if(message.length == 0) {
@@ -64,6 +69,10 @@ function objectEquals(obj1, obj2) {
     }
     return true;
 }
+
+function between(a,b,c) {
+	return a <= b && b < c;
+} 
 
 // act on input change
 $('#message').bind('input propertychange', function() { updateVisuals(); });
